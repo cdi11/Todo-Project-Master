f318b97210fc957853916675867bf963067654ae
Charlie Dill <charliedill@charlies-macbook-air.local>
2018-10-15 21:49:45 -0500

    Initial Commit

---
+++ Todo-Project-Master/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo-Project-Master
+//
+//  Created by Charlie Dill on 10/15/18.
+//  Copyright © 2018 dill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
